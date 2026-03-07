551e4c209f0a232e6790ad91cc79478fe4acb29c
asi3946 <yuuma218eldol@gmail.com>
2026-03-07 22:13:26 +0900

    reafactor: ./store/vimの作成　#25

--- src/store/models.ts
+++ src/store/models.ts
@@ -1,4 +1,6 @@
 // src/store/models.ts
 export * from "./editorAtom";
 export * from "./memoAtom";
-export * from "./vimAtom";
+export * from "./vim/core";
+export * from "./vim/edit";
+export * from "./vim/movement";
